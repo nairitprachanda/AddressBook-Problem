ed7c3373535dd3ea5985d8f2efe93880cac135e4
nairitprachanda <nairitprachanda@gmail.com>
2020-10-06 22:02:38 +0530

    [nairit] refactor: creating a contact

--- Address-Book/Address-Book/Program.cs
+++ Address-Book/Address-Book/Program.cs
@@ -7,6 +7,9 @@ namespace Address_Book
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book");
+            Address_book address_book = new Address_book();
+            address_book.AddContact ("nairit", "prachanda", "Sivaji nagar", "visakhapatnam", "AP", 530046, 8179263777, "nanana") ;
+            address_book.ViewContact();
         }
     }
 }
