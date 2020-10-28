ef8a71eb1da6898f12d05efc8efa988469273dc3
nairitprachanda <nairitprachanda@gmail.com>
2020-10-28 23:16:06 +0530

    [nairit] refactor: adding new contact

---
+++ Address-Book/Address-Book/AddressBookSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class AddressBookSystem
+    {
+        public List<Contact> ContactList;
+        public AddressBookSystem()
+        {
+            this.ContactList = new List<Contact>();
+        }
+        public void AddContact(Contact obj)
+        {
+            this.ContactList.Add(obj);
+        }
+    }
+}
--- Address-Book/Address-Book/Program.cs
+++ Address-Book/Address-Book/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
 
-namespace Address_Book
+namespace AddressBook
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book");
-            Address_book address_book = new Address_book();
-            address_book.AddContact ("nairit", "prachanda", "Sivaji nagar", "visakhapatnam", "AP", 530046, 8179263777, "nanana") ;
-            address_book.ViewContact();
+            Console.WriteLine("Welcome To Address Book Program on Master Branch ");
+            Console.WriteLine("\n");
+            Console.WriteLine("Add the Contact Details ");
+            Console.WriteLine("\n");
+            AddressBookSystem addressbook = new AddressBookSystem();
+            Contact contact = new Contact();
+            SetDetails(contact);
+            addressbook.AddContact(contact);
+        }
+        public static void SetDetails(Contact contact)
+        {
+            Console.WriteLine("First Name : ");
+            contact.FirstName = Console.ReadLine();
+            Console.WriteLine("Last Name :");
+            contact.LastName = Console.ReadLine();
+            Console.WriteLine("Address :");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("City :");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("State :");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Zip Code :");
+            contact.Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Phone Number :");
+            contact.PhoneNumber = long.Parse(Console.ReadLine());
+            Console.WriteLine("Email Address :");
+            contact.Email = Console.ReadLine();
         }
     }
+
 }
